27b20560a226804088588332dd3af108cc0ddd37
gaurav <dobriyal516@gmail.com>
2025-07-02 20:29:22 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isGithubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
+const repo = 'MovieTracker'; 
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+const nextConfig = {
+  output: 'export', // Required for static export
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
